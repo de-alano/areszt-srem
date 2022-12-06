d69c4db3c2bc49bacf9eeb0820eecf2799165eff
Alan Dominiczak <alan.dominiczak10@gmail.com>
2022-12-06 22:07:07 +0100

    fix js bug

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,16 @@
-const init = (() => {
-    // HAMBURGER MENU
-    const menuIcon = document.querySelector('.header__hamburger');
-    const nav = document.querySelector('.mobile-navigation');
-    const navLinks = [...document.querySelectorAll('.mobile-navigation__link')];
+// HAMBURGER MENU
+const menuIcon = document.querySelector('.header__hamburger');
+const nav = document.querySelector('.mobile-navigation');
+const navLinks = [...document.querySelectorAll('.mobile-navigation__link')];
 
-    menuIcon.addEventListener('click', () => {
-        menuIcon.classList.toggle('change');
-        nav.classList.toggle('change');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('change');
+    nav.classList.toggle('change');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('change');
+        menuIcon.classList.remove('change');
     });
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            nav.classList.remove('change');
-            menuIcon.classList.remove('change');
-        });
-    });
-
-})();
+});
